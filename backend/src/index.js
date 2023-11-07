e9365b292c7cceba803ce5e0884ea91b3c19cd09
Jonathan JG <jonysthil@gmail.com>
2023-11-07 13:36:47 -0600

    datos para el env

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,9 +1,20 @@
-import express from 'express';
+//import app from "./app.js";
+import express from "express";
+import { pool } from "./database.js";
 
 const app = express();
 
 app.get('/', (req, res) => {
-    res.send('creo que ya!! jajaja');
+    res.send('Hello world');
+});
+
+app.get("/ping", async (req, res) => {
+    try {
+        const [result] = await pool.query(`SELECT NOW() as now`);
+        return res.json(result[0]);
+    } catch (error) {
+        console.log(error);
+    }
 });
 
 app.listen(8080);
